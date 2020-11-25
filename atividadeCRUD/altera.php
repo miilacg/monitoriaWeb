2be32c70979deeec7754c48cf2a39f0ed788debd
Camila <camiilacguimaraes@gmail.com>
2020-11-25 12:32:18 -0300

    Alteração dos dados

--- atividadeCRUD/altera.php
+++ atividadeCRUD/altera.php
@@ -4,10 +4,29 @@
     include 'acessoBanco.php';
 
     $excluir = filter_input (INPUT_POST, 'excluir_id', FILTER_SANITIZE_STRING);   
-    echo $excluir;
-    $delete = "DELETE FROM usuario WHERE id = '$excluir';";
-    $d = mysqli_query($conn, $delete);
+    $alterar = filter_input (INPUT_POST, 'alterar_id', FILTER_SANITIZE_STRING);
 
-    mysqli_close($conn);    
-    header ("Location: listar.php");
+    if ($excluir != null && $excluir != 0){
+        $delete = "DELETE FROM usuario WHERE id = '$excluir';";
+        $d = mysqli_query($conn, $delete);
+        header ("Location: listar.php");
+    }else{
+        if ($alterar != null && $alterar != 0){
+            $nome = filter_input (INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+            $email = filter_input (INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+            $sexo = filter_input (INPUT_POST, 'sexo', FILTER_SANITIZE_STRING);
+            $numAlistamento = filter_input (INPUT_POST, 'numAlistamento', FILTER_SANITIZE_STRING);
+
+            if($sexo == "Masculino"){
+                $update = "UPDATE `usuario` SET `nome_usuario`='$nome',`email`='$email',`sexo`='$sexo',`numero_alistamento`='$numAlistamento' WHERE id = '$alterar'";
+            }else{
+                $update = "UPDATE `usuario` SET `nome_usuario`='$nome',`email`='$email',`sexo`='$sexo' WHERE id = '$alterar'";
+
+            }
+            $u = mysqli_query($conn, $update);
+            header ("Location: listar.php");
+        }
+    }
+    
+    mysqli_close($conn);      
 ?>
--- atividadeCRUD/listar.php
+++ atividadeCRUD/listar.php
@@ -12,10 +12,6 @@
     }  
 ?>
 
-<script>
- 
-</script>
-
 <!DOCTYPE HTML>
 <html lang="pt-br">
 	<head>
@@ -38,15 +34,15 @@
                 <div class="modal-content">
                     <form method="POST" action="altera.php" id="update_tracking_number_form">                
                         <script>
-                            function marcar(id){
+                            function marcarExcluir(id){
                                 var divPai = $('.opcao');
                                 var input = document.createElement('input');
                                 input.setAttribute('type', 'radio'); 
-                                input.setAttribute('name', 'excluir_id');
+                                input.setAttribute('name', 'excluir_id');                                
                                 input.setAttribute('value', id); 
                                 input.setAttribute('checked', 'checked');  
                                 divPai.append(input);                                 
-                            }                            
+                            }                                  
                         </script>                        
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel">Você tem certeza que deseja excluir o cadastro?</h5>
@@ -62,33 +58,70 @@
         </div> 
         
         <div class="modal fade" id="modalAlterar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
+            <div class="modal-dialog" role="document" onblur="excluir();">
                 <div class="modal-content">
-                    <form method="POST" action="./altera.php">
-                        <div class="modal-header">
-                        <?php
-                            $id = filter_input (INPUT_POST, 'numId', FILTER_SANITIZE_STRING);  
-                            include 'acessoBanco.php';
+                    <form method="POST" action="altera.php">
+                        <script>
+                            function marcarAlterar(id, alistamento){                                
+                                var divPai = $('.opcao');
+                                var input = document.createElement('input');
+                                input.setAttribute('type', 'radio'); 
+                                input.setAttribute('name', 'alterar_id');                                
+                                input.setAttribute('value', id); 
+                                input.setAttribute('checked', 'checked');  
+                                divPai.append(input); 
+                                
+                                numero = Number(Object(alistamento));
 
-                            $busca = "SELECT * FROM usuario WHERE id = '$id'";	          
-                            $verifica = mysqli_query($conn, $busca);
-                            $linha = mysqli_fetch_assoc($verifica);           
-                        ?>
+                                if (alistamento != null && alistamento != 0){
+                                    var alistamento = document.getElementById('alistamento');
+                                    var inputAlistamento = document.createElement('input');
+                                    alistamento.setAttribute('style', 'display: block');
+                                    inputAlistamento.setAttribute('class', 'form-control alistamento'); 
+                                    inputAlistamento.setAttribute('type', 'number');  
+                                    inputAlistamento.setAttribute('id', 'numAlistamento');
+                                    inputAlistamento.setAttribute('name', 'numAlistamento');     
+                                    inputAlistamento.setAttribute('value', numero);   
+                                    alistamento.append(inputAlistamento); 
+                                }
+                            }     
+                            
+                            function excluir(){
+                                var alistamento = document.getElementById('alistamento');
+                                alistamento.setAttribute('style', 'display: none');
+                                
+                                document.getElementById('numAlistamento').remove()
+                            }
+                        </script>
+                        <div class="modal-header">                        
                             <h5 class="modal-title" id="exampleModalLabel">Alterar dados</h5>                    
                         </div>
-                        <div class="modal-body">
+                        <div class="opcao" style="display:none"></div>                        
+                        <div class="modal-body">     
                             <div class="form-group">
-                                <label for="nome" class="col-form-label">Nome do usuário:</label>
-                                <input required type="text" class="form-control" id="nome" value="<?php echo $linha['nome_usuario']; ?>">
+                                <label for="name" class="col-form-label">Nome do usuário:</label>
+                                <input type="text" class="form-control name" id="name" name="nome" required>
                             </div>
                             <div class="form-group">
-                                <label for="message-text" class="col-form-label">E-mail:</label>
-                                <input type="e-mail" class="form-control" name="email" id="email" value="<?php echo $linha['email']; ?>" required>  
-                            </div>                            
+                                <label for="email" class="col-form-label">E-mail:</label>
+                                <input type="e-mail" class="form-control email" id="email" name="email" required>
+                            </div>   
+                            <div class="form-group">
+                                <label for="sexo">Sexo</label><br>
+                                <select class="form-control sexo" id="sexo" name="sexo" required><br>
+                                    <option value="">Escolha uma opção</option>
+                                    <option value="Feminino">Feminino</option>
+                                    <option value="Masculino">Masculino</option>
+                                    <option value="Prefiro não informar">Prefiro não informar</option>                              
+                                </select>
+                            </div> 
+                            <div class="form-group" id="alistamento" style="display:none">
+                                <label for="alistamento" class="col-form-label">Número do alistamento:</label>
+                            </div>                           
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                            <input type="submit" class="btn btn-primary" name="entrar" value="Enviar"/>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="excluir();">Fechar</button>
+                            <input type="submit" class="btn btn-primary" name="entrar" value="Alterar"/>
                         </div>
                     </form>
                 </div>
@@ -114,7 +147,6 @@
                             <tbody class="corpoTabela">
                                 <?php
                                     include 'acessoBanco.php';
-
                                     $busca = "SELECT * FROM usuario";	          
                                     $verifica = mysqli_query($conn, $busca);
                                     $linha = mysqli_fetch_assoc($verifica);  
@@ -123,8 +155,8 @@
                                         ?>
                                             <tr>                                                
                                                 <td><label type="text"><?php echo $linha['nome_usuario'];?></td>
-                                                <td><button type="button" class="btn btn-primary listar" data-toggle="modal" data-target="#modalAlterar">Alterar</button></td>
-                                                <td><button type="button" class="btn btn-secondary listar" data-toggle="modal" data-target="#modalExcluir" onclick="marcar('<?php echo $linha['id']; ?>');">Excluir</button></td>	                                                    
+                                                <td><button type="button" class="btn btn-primary listar" data-toggle="modal" data-target="#modalAlterar" data-name="<?php echo $linha['nome_usuario'];?>" data-sexo="<?php echo $linha['sexo'];?>" data-email="<?php echo $linha['email'];?>" onclick="marcarAlterar('<?php echo $linha['id']; ?>', '<?php echo $linha['numero_alistamento']; ?>');">Alterar</button></td>
+                                                <td><button type="button" class="btn btn-secondary listar" data-toggle="modal" data-target="#modalExcluir" onclick="marcarExcluir('<?php echo $linha['id']; ?>');">Excluir</button></td>	                                                    
                                             </tr>                                                    
                                         <?php
                                     }while($linha = mysqli_fetch_assoc($verifica));
@@ -148,3 +180,17 @@
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
     </body>
 </html> 
+
+<script>
+    $('#modalAlterar').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Botão que acionou o modal
+        var email = button.data('email') // Extrai informação dos atributos data-*
+        var nome = button.data('name')
+        var sexo = button.data('sexo')
+           
+        var modal = $(this)
+        modal.find('.modal-body input.name').val(nome)
+        modal.find('.modal-body input.email').val(email)
+        modal.find('.modal-body select.sexo').val(sexo)          
+    }) 
+</script>
